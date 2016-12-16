e3070039b5288e7d80be177bd1c1b0a7ce590820
Kishor SS <kishor.sathigrama@gmail.com>
2016-12-16 13:47:23 +0530

    added new event and upcoming events

---
+++ html/drawing_comp.php
@@ -0,0 +1,37 @@
+<body>
+
+<!-- Navigation-->
+<?php
+include ('includes/header_inner.php');
+?>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="page-header">
+				<h2> ಚಿತ್ರ ಕಲಾ ಸ್ಪರ್ಧೆ </h2>
+			</div>
+		</div>
+		<div class="col-sm-12 center">
+		<p>ದಿನಾಂಕ 11<i>-</i>9<i>-</i>2016 ರಂದು, ಹೊಯ್ಸಳ ಕರ್ನಾಟಕ ಸಂಘದ ಆವರದಲ್ಲಿ ಇಷ್ರೇ, ಸಿಎಂಸಿಎ ಮತ್ತು ಹೊಯ್ಸಳ ಕರ್ನಾಟಕ ಸಂಘದ ಸಹಯೋಗದೊಂದಿಗೆ ಶನಿವಾರ ಚಿತ್ರ ಕಲಾ ಸ್ಪರ್ಧೆಯನ್ನು ಏರ್ಪಡಿಸಲಾಗಿತ್ತು. ನಾನಾ ಶಾಲೆಯ ೧೦೩ ವಿಧ್ಯಾರ್ಥಿಗಳು ಭಾಗವಹಿಸಿದ್ದರು. ವಿಜೇತರಿಗೆ ಪಿ. ವಿ. ಭಾರದ್ವಾಜ ಮತ್ತು ಸೆಲ್ವಿರಾಜ್ ಬಹುಮಾನ ವಿತರಿಸಿದರು.</p>
+		<img src="image/drawing/001.jpg" alt="" class="img-responsive img-thumbnail"><br>
+		<img src="image/drawing/002.jpg" alt="" class="img-responsive img-thumbnail"><br/>
+		</div>
+		
+
+	</div>	
+
+
+
+
+</div>
+	
+<?php
+include ('includes/footer.php');
+?>
+
+
+
+
+
+</body>
+
--- html/event.php
+++ html/event.php
@@ -4,50 +4,61 @@
 <?php
 include ('includes/header_inner.php');
 ?>
+
 <div class="container">
 	<div class="row">
-		<div class="col-sm-12">
-			<div class="page-header">
-				<h2>ಕಾರ್ಯಕ್ರಮಗಳು</h2>
-			</div>
+		<div class="col-sm-4">
+			<h3>Post Event</h3>
+			<blockquote>
+				<a href="shanar_janti.php"><p>14<i>-</i>5<i>-</i>2016 ಶಂಕರ ಜಯಂತಿ</p></a>
+				<footer>ನವೀಕೃತ ಹೊಯ್ಸಳ ಭವನ</footer>
+			</blockquote>
+			<blockquote>
+				<a href="cvsy.php" ><p>19<i>-</i>6<i>-</i>2016 ಚಾಮುಂಡಿ ವೃಕ್ಷ ಸಂವರ್ದಿನಿ ಯೋಜನ</p></a>
+				<footer>ಚಾಮುಂಡಿ ಬೆಟ್ಟ</footer>
+			</blockquote>
+			<blockquote>
+				<a href="sks.php"><p>24<i>-</i>7<i>-</i>2016 ಸಾಂಸ್ಕೃತಿಕ ಮತ್ತು ಕ್ರೀಡಾ ಸ್ಪರ್ಧೆಗಳು</p></a>
+				 <footer>ಹೊ. ಕ. ಸಂಘ ಮುಖ್ಯ ಕಟ್ಟಡ</footer>
+			</blockquote>
+			<blockquote>
+				<a href="sks.php"><p>31<i>-</i>7<i>-</i>2016 ಸಂಸ್ಥಾಪಕರ ದಿನಾಚರಣೆ ಹಾಗೂ ಹೊಯ್ಸಳ ಹಬ್ಬ</p></a>
+				 <footer>ಶಾರದ ವಿಳಾಸ ಕಾಲೇಜು ಆವರಣ</footer>
+			</blockquote>
+			<blockquote>
+				<a href="ganapati.php"><p>11<i>-</i>09<i>-</i>2016 ಶ್ರೀ ಮಹಾಗಣಪತಿಯ ಮಂಗಳಾರತಿ ಕಾರ್ಯಕ್ರಮ </p></a>
+				 <footer>ನವೀಕೃತ ಹೊಯ್ಸಳ ಭವನ</footer>
+			</blockquote>
+			<blockquote>
+				<a href="drawing_comp.php"><p>10<i>-</i>12<i>-</i>2016 ಚಿತ್ರ ಕಲಾ ಸ್ಪರ್ಧೆ </p></a>
+				 <footer>ಶ್ರೀ ಶ್ರೀ ಚಂದ್ರಶೇಖರೇಂದ್ರ ಸರಸ್ವತಿ ಶತಮಾನೋತ್ಸವ ಭವನ</footer>
+			</blockquote>
 		</div>
-		<div class="col-sm-12">
-		<blockquote>
-		  <a href="shanar_janti.php"><p>14<i>-</i>5<i>-</i>2016 ಶಂಕರ ಜಯಂತಿ</p></a>	
-		  <footer>ನವೀಕೃತ ಹೊಯ್ಸಳ ಭವನ</footer>
-		</blockquote>
-		<blockquote>
-		 <a href="cvsy.php" <p>19<i>-</i>6<i>-</i>2016 ಚಾಮುಂಡಿ ವೃಕ್ಷ ಸಂವರ್ದಿನಿ ಯೋಜನ</p></a>
-		  <footer>ಚಾಮುಂಡಿ ಬೆಟ್ಟ</footer>
-		</blockquote>
-		<blockquote>
-		 <a href="sks.php" <p>24<i>-</i>7<i>-</i>2016 ಸಾಂಸ್ಕೃತಿಕ ಮತ್ತು ಕ್ರೀಡಾ ಸ್ಪರ್ಧೆಗಳು</p></a>
-		  <footer>ಹೊ. ಕ. ಸಂಘ ಮುಖ್ಯ ಕಟ್ಟಡ</footer>
-		</blockquote>
-		<blockquote>
-		 <a href="sks.php" <p>31<i>-</i>7<i>-</i>2016 ಸಂಸ್ಥಾಪಕರ ದಿನಾಚರಣೆ ಹಾಗೂ ಹೊಯ್ಸಳ ಹಬ್ಬ</p></a>
-		  <footer>ಶಾರದ ವಿಳಾಸ ಕಾಲೇಜು ಆವರಣ</footer>
-		</blockquote>
-		<blockquote>
-		 <a href="ganapati.php" <p>11<i>-</i>09<i>-</i>2016 ಶ್ರೀ ಮಹಾಗಣಪತಿಯ ಮಂಗಳಾರತಿ ಕಾರ್ಯಕ್ರಮ </p></a>
-		  <footer>ನವೀಕೃತ ಹೊಯ್ಸಳ ಭವನ</footer>
-		</blockquote>
-		
+		<div class="col-sm-4">
+			<h3>Upcoming Event</h3>
+				<blockquote>
+				<p>ದಿನಾಂಕ 01<i>-</i>01<i>-</i>2017 ರಂದು ಹೊಸ ವರುಷದ ಪ್ರಯುಕ್ತ <b>ಬೆಳದಿಂಗಳ (ದಕ್ಷಿಣ ಕನ್ನಡ ಜಿಲ್ಲೆಯ ಶೈಲಿಯ)  ಊಟವನ್ನು ಸಂಜೆ 6.30ಕ್ಕೆ  </b>ಏರ್ಪಡಿಸಲಾಗಿದೆ. ಸಂಘದ ಸದಸ್ಯರು ರೂ ೬೦ ನ್ನು ದಿನಾಂಕ 29.02.2016ರ ಒಳಗೆ ಪಾವತಿಸಿ ನೊಂದಾಯಿಸಿಕೊಳ್ಳಬೇಕು.</p>
+			</blockquote>
+			<blockquote>
+				<p>ದಿನಾಂಕ 15<i>-</i>01<i>-</i>2017 ರಂದು ಸಂಘದ ಶತಮಾನೋತ್ಸವ ಭವನದಲ್ಲಿ<b> ಶ್ರೀ ಸತ್ಯನಾರಾಯಣ ಪೂಜೆ, ಸುದರ್ಶನ ಹೋಮ ಮತ್ತು ವೈದಕೀಯ ತಪಾಸಣೆ</b>ಯನ್ನು ನಾರಾಯಣ ಹೃದಯಾಲಯದ ವತಿಯಿಂದ ಏರ್ಪಡಿಸಲಾಗಿದೆ.</p>
+			</blockquote>
 		</div>
-
-	</div>
-
-
-
-
+		<div class="col-sm-4">
+			<h3>ಶ್ರದ್ಧಾಂಜಲಿ</h3>
+			<blockquote>
+				<p>ದಿನಾಂಕ 06<i>-</i>12<i>-</i>2016 ರಂದು ಸಂಘದ ದಾನಿಗಳಾದ ಹೆಚ್. ಆರ್ ಲಕ್ಷ್ಮೀಕಾಂತ್ ರವರು ಸ್ವರ್ಗಸ್ಥರದರು.</p>
+			</blockquote>
+			<blockquote>
+				<p>ದಿನಾಂಕ 12<i>-</i>12<i>-</i>2016 ರಂದು ಸಂಘದ ಕಾರ್ಯಕಾರಿ ಸಮಿತಿಯ ಸದಸ್ಯರಾದ ಎನ್. ಎಸ್. ರವಿ ಅವರು ಸ್ವರ್ಗಸ್ಥರದರು.</p>
+			</blockquote>
+			<p>ಇವರ ಆತ್ಮಕ್ಕೆ ಶಾಂತಿಸಿಗಲಿ ಎಂದು ಕಾರ್ಯಕಾರಿ ಸಮಿತಿ ವತಿಯಿಂದ ಪ್ರಾರ್ಥಿಸುತ್ತಿದ್ದೆವೆ.</p>
+		</div>
+		</div>
 </div>
 	
 <?php
 include ('includes/footer.php');
 ?>
-
-
-
 
 
 </body>
