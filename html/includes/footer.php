b2d007d299f9179e1f396a72de9e02d64454e522
Kishor SS <kishor.sathigrama@gmail.com>
2017-01-07 11:51:05 +0530

    new year event added

--- html/includes/footer.php
+++ html/includes/footer.php
@@ -3,7 +3,7 @@
 		<div class="col-sm-12 ">
 			<div class="center">
 				<hr />
-				<span class="glyphicon glyphicon-copyright-mark"></span> 2016 ಹೊಯ್ಸಳ ಕರ್ನಾಟಕ ಸಂಘ, ಮೈಸೂರು. ಎಲ್ಲ ಹಕ್ಕುಗಳನ್ನೂ ಕಾಯ್ದಿರಿಸಲಾಗಿದೆ.
+				<span class="glyphicon glyphicon-copyright-mark"></span> 2017 ಹೊಯ್ಸಳ ಕರ್ನಾಟಕ ಸಂಘ, ಮೈಸೂರು. ಎಲ್ಲ ಹಕ್ಕುಗಳನ್ನೂ ಕಾಯ್ದಿರಿಸಲಾಗಿದೆ.
 			</div>
 			
 <script type="text/javascript" src="http://arrow.scrolltotop.com/arrow71.js"></script>
---
+++ html/new_year.php
@@ -0,0 +1,39 @@
+<body>
+
+<!-- Navigation-->
+<?php
+include ('includes/header_inner.php');
+?>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="page-header">
+				<h2>ಹೊಸ ವರ್ಷದ ಆಚರಣೆ</h2>
+			</div>
+		</div>
+		<div class="col-sm-12 center">
+		<p>ದಿನಾಂಕ 1<i>-</i>1<i>-</i>2017 ರಂದು, ಹೊಸ ವರ್ಷದ ಮೊದಲನೆಯ ದಿವಸ, ಸಂಘದಲ್ಲಿ ವಿಶಿಷ್ಟ ಕಾರ್ಯಕ್ರಮಗಳು	</p>
+		<img src="image/new_year/001.jpg" alt="" class="img-responsive img-thumbnail"><br>
+		<img src="image/new_year/002.jpg" alt="" class="img-responsive img-thumbnail"><br/>
+		<img src="image/new_year/003.jpg" alt="" class="img-responsive img-thumbnail"><br/>
+		<img src="image/new_year/004.jpg" alt="" class="img-responsive img-thumbnail">
+		</div>
+		
+
+	</div>	
+
+
+
+
+</div>
+	
+<?php
+include ('includes/footer.php');
+?>
+
+
+
+
+
+</body>
+
